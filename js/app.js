583a23f55853348773505fc6530ca5b1616d1490
unknown <akang2@gmail.com>
2014-08-26 13:53:12 -0700

    adding the hadouken animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
 	$('.ryu').on('mouseenter', function() {
-		alert('yep');
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.on('mouseleave', function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.on('mousedown', function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show()
+			.animate({
+				'left': '300px'
+			},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+			);
+	})
+	.on('mouseup', function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		$('.hadouken').hide();	
 	});
 });
 
