3bae3ec13279aa2fcf8b28d035a7965077742367
unknown <akang2@gmail.com>
2014-08-28 04:10:32 -0700

    figured out why the fireball works here. it's cause the audio element was before the hadouken class element

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.on('mousedown', function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show()
+		$('.hadouken').finish().show()
 			.animate({
 				'left': '300px'
 			},
@@ -28,3 +29,8 @@ $(document).ready(function(){
 	});
 });
 
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
